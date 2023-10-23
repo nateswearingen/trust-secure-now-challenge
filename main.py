56aeb5bc39413a062d95af919a27e9a3909d35a8
nateswearingen <142051584+nateswearingen@users.noreply.github.com>
2023-10-22 22:41:52 -0500

    add_cust is together and working (with room for improvement)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+
 from flask import Flask
 from flask import request
 app = Flask(__name__)
@@ -11,7 +12,7 @@ except mysql.connector.Error as err:
     print(err)
     exit("Couldn't establish db connection")
 else:
-    print("Connection established!")
+    print("DB Connection established!")
 
 
 def close_db_connection(sender, **extra):
@@ -21,8 +22,65 @@ def close_db_connection(sender, **extra):
 
 @app.route("/add_cust", methods=['POST', 'GET'])
 def add_cust():
-    #TODO
-    return
+    ''' Api for adding a customer
+    Extra fields are ignored.
+
+    This currently doesn't support json inputs, but it needs to
+    '''
+    # get the columns in the customer table
+    cursor = cnx.cursor(buffered=True)
+    query = "select * from customer c limit 1"
+    cursor.execute(query)
+    data = {}
+    for column, data_type in [(x[0], x[1]) for x in cursor.description]:
+        if column == "id": #don't allow inserting the ID, as it's maintained by the database
+            continue
+        if column in request.form:
+            field = request.form[column]
+        else:
+            field = request.args.get(column, '')
+        if field is None or field == '':
+            continue
+        match mysql.connector.FieldType.get_info(data_type):
+            case "VARCHAR":
+                field = "'" + field + "'"
+            case "VAR_STRING":
+                field = "'" + field + "'"
+            #TODO Insert support for the date datatype here, and whatever else gets implemented
+        data[column] = field
+    cursor.close()
+
+    #business validations
+    out = ""
+    if "email" in data or "phone" in data or "address" in data:
+        pass
+    else:
+        out += "At least one of phone, email, or address is required"
+    if not "name" in data:
+        out += "Name is a required field"
+    if len(out) > 0:
+        return out
+
+    #build insert string
+    query = "Insert into customer ("
+    vals = ") values ("
+    #vulnerable to SQL injection attack (out of time to rework)
+    i = 0
+    for k, v in data.items():
+        if i > 0:
+            query += ", "
+            vals += ", "
+        query += k
+        vals += v
+        i += 1
+    query += vals + ")"
+
+    cursor = cnx.cursor(buffered=True)
+    cursor.execute(query)
+    cnx.commit()
+    out = str(cursor.rowcount) + " record(s) added."
+    cursor.close()
+    return out
 
 
 @app.route("/update_cust", methods=['POST', 'GET'])
@@ -34,9 +92,10 @@ def update_cust():
 @app.route("/delete_cust", methods=['POST', 'GET'])
 def delete_cust():
     #fetch id
-    if id in request.form:
+    if 'id' in request.form:
         cust_id = request.form['id']
-    cust_id = request.args.get('id', '')
+    else:
+        cust_id = request.args.get('id', '')
     if cust_id is None or cust_id == '':
         return "<p>You must supply a customer id.</p>"
 
@@ -62,9 +121,10 @@ def delete_cust():
 @app.route("/get_cust")
 def get_cust():
     #fetch id
-    if id in request.form:
+    if 'id' in request.form:
         cust_id = request.form['id']
-    cust_id = request.args.get('id', '')
+    else:
+        cust_id = request.args.get('id', '')
     if cust_id is None or cust_id == '':
         return "<p>You must supply a customer id.</p>"
 
